77ab76c6b238ff37c6a9c8ff9727e14fe5096930
Ayush Deore <75108109+Weebo-xD@users.noreply.github.com>
2022-03-23 16:12:13 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -4,12 +4,12 @@ class Config(object):
     
     DOWNLOAD_LOCATION = "./DOWNLOADS"
 
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5092511287:AAFUfE-_ZCBezMUhrzSTiinMW52XRppKYr8")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
-    APP_ID = int(os.environ.get("APP_ID", 1995654))
+    APP_ID = int(os.environ.get("APP_ID", 12345))
 
-    API_HASH = os.environ.get("API_HASH", "91aa713a74d1bccb50cd1c03758316bf")
+    API_HASH = os.environ.get("API_HASH", "")
 
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "860540443").split())
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
   
     
